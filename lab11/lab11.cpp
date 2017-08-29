b7f4a0b071b77d06aee692eb3d16cbae9fdee2e2
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2017-08-29 22:26:34 +0000

    Lab 1.1 Commit

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,7 +1,7 @@
 Pick a number between 1 and 50 (inclusive)
 Prompt user to guess the number
 if guess is correct, the user wins! Their score is 10 points, minus one point for each incorrect guess
-if guess is higher than number, say "Guess Lower" and return to step 2.
-if guess is lower than number, say "Guess Higher" and return to step 2.
+if guess is higher than number, say "Guess Lower" and return to step 2
+if guess is lower than number, say "Guess Higher" and return to step 2
 if guess is not between 1 and 50, say "Sorry, you must guess a valid number"
 if user does not guess number after 10 attempts, say "Game Over. Try again?"
