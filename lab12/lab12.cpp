e26784f36708b15da69e1ce89f5f8674d54eab1c
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2017-08-29 22:01:13 +0000

    Lab 1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,15 @@
+//Created By: Isaac Vander Sluis
+//Created On: 8/29/17
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "  ^      ^  " << endl;
+    cout << " / \\~~~~/ \\" << endl;
+    cout << "(  6   6   )" << endl;
+    cout << " (/ V    \\) " << endl;
+    cout << "  88888888  " << endl;
+    cout << "   ^^  ^^   " << endl;
+} //It's an owl.
