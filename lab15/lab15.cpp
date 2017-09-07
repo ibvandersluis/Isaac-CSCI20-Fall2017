9ad4d157e8400287736dcbae7733a1873517429a
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2017-09-07 21:45:39 +0000

    Lab 1.5 Submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,41 @@
+//Created By: Isaac Vander Sluis
+//Created On: 9/7/17
+
+/*  ~~~~MAD LIBS~~~~
+    This program has a pre-determined 'mad libs' template. The passage
+    is from The Martian; words have been deleted from it, and the user
+    will input their own words to fill those places. The result will be
+    similar to the original passage, but with nonsensical insertions from
+    an oblivious user.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string element = "potassium";               //initializing my string variabls to values
+    string present_verb = "popping";            //I got from my girlfriend when I tested it on her
+    string natural_disaster = "tsunami";
+    string noun = "peacock";
+    string adverb = "awkwardly";
+    
+    cout << "Enter an element: " << endl;       //getting string values from the user
+    getline(cin, element);
+    cout << "Enter a verb in the present tense: " << endl;
+    getline(cin, present_verb);
+    cout << "Enter a natural disaster or intense weather condition: " << endl;
+    getline(cin, natural_disaster);
+    cout << "Enter a noun: " << endl;
+    getline(cin, noun);
+    cout << "Enter an adverb: " << endl;
+    getline(cin, adverb);
+                                                //outputting the mad lib with variables replacing missing words
+    cout << "I awoke to the " << element << " alarm in my suit. A steady, obnoxious " << present_verb << endl;
+    cout << "that eventually roused me from a deep and profound desire to just " << endl;
+    cout << "fucking die. The " << natural_disaster << " had abated. I was facedown, almost totally buried " << endl;
+    cout << "in " << noun << ". As I " << adverb << " came to, I wondered why I wasn’t more dead." << endl;
+    
+    return 0;
+}
