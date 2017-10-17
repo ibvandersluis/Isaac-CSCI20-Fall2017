024047b7946fbecae48439ea8d24090ff7af9cca
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2017-10-17 23:00:30 +0000

    2.0 Assignment - Race Splitter Submission

--- racesplitter/racesplitter.cpp
+++ racesplitter/racesplitter.cpp
@@ -14,63 +14,37 @@ using namespace std;
 
 class Runner {
     public:
-        void SetSeconds(int minutes, int seconds);  //mutator
-        void SetFPS(int seconds);                   //mutator
-        void SetMPS(int seconds);                   //mutator
-        void SetLap();                              //mutator
-        
-        int GetSeconds() const;                     //accessor
-        double GetFPS() const;                      //accessor
-        double GetMPS() const;                      //accessor
-        double GetLap() const;                      //accessor
+        void SetSeconds(double minutes, double seconds);  //mutator
+        double GetSeconds() const;                        //accessor
     private:
-        double secs_;                               //private class variables
-        double fps_;
-        double mps_;
-        double lap_;
+        double secs_;                                     //private class variable
 };
 
 //declaring all class functions
-void Runner::SetSeconds(int minutes, int seconds) {
+
+/* This function takes a time interval in minutes and seconds and converst it
+   to seconds.
+   Input: two integers, double minutes and double seconds
+   Output: stores value in secs_
+*/
+void Runner::SetSeconds(double minutes, double seconds) {
     secs_ = (minutes * 60.0) + seconds;
 }
 
-void Runner::SetFPS(int seconds) {
-    fps_ = 5280.0 / GetSeconds();
-}
-
-void Runner::SetMPS(int seconds) {
-    mps_ = 1600.0 / GetSeconds();
-}
-
-void Runner::SetLap() {
-    lap_ = GetSeconds() / 4.0;
-}
-
-int Runner::GetSeconds() const {
+/* This function returns the value stored in secs_.
+   Input: the variable name secs_
+   Output: the value stored in secs_
+*/
+double Runner::GetSeconds() const {
     return secs_;
 }
 
-double Runner::GetFPS() const {
-    return fps_;
-}
-
-double Runner::GetMPS() const {
-    return mps_;
-}
-
-double Runner::GetLap() const {
-    return lap_;
-}
 
 int main() {
     Runner runner1;                                 //declaring object of type Runner
-    int user_mins = 0;                              //initializing uner input variables
+    int user_mins = 0;                              //initializing user input variables
     int user_secs = 0;
     double first_place_time = 253.0;                //defining winner's race time in seconds
-    double first_fps = 5280.0 / first_place_time;   //calculating winner's pace in ft/s
-    double first_mps = 1600.0 / first_place_time;   //calculating winner's pace in m/s
-    double first_lap = first_place_time / 4;        //calculating winner's avg lap time in seconds
     
     cout << "Enter your race time." << endl;        //getting runner info from user
     cout << "Minutes: " << endl;
@@ -78,18 +52,15 @@ int main() {
     cout << "Seconds: " << endl;
     cin >> user_secs;
     
-    runner1.SetSeconds(user_mins, user_secs);       //using all mutators to assign values to class variables
-    runner1.SetFPS(runner1.GetSeconds());
-    runner1.SetMPS(runner1.GetSeconds());
-    runner1.SetLap();
+    runner1.SetSeconds(user_mins, user_secs);       //using mutator to assign value to class variable
     
     cout << endl;                                   //outputting results to user
-    cout << "You ran at a velocity of " << runner1.GetFPS() << " feet/second, ";
-    cout << "or " << runner1.GetMPS() << " meters/second." << endl;
-    cout << "The race champion ran at " << first_fps << " feet/second, ";\
-    cout << "or " << first_mps << " meters/second." << endl;
-    cout << "Your 1/4 mile lap time was " << runner1.GetLap() << " seconds, compared with";
-    cout << " the champions lap time of " << first_lap << " seconds." << endl;
+    cout << "You ran at a velocity of " << 5280.0 / runner1.GetSeconds() << " feet/second, ";
+    cout << "or " << 1600.0 / runner1.GetSeconds() << " meters/second." << endl;
+    cout << "The race champion ran at " << 5280.0 / first_place_time << " feet/second, ";\
+    cout << "or " << 1600.0 / first_place_time << " meters/second." << endl;
+    cout << "Your 1/4 mile lap time was " << runner1.GetSeconds() / 4.0 << " seconds, compared with";
+    cout << " the champions lap time of " << first_place_time / 4 << " seconds." << endl;
     
     return 0;
 }
