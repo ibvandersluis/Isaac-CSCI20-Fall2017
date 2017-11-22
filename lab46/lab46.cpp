76e74d40dafc70554e2665db3e11c8181481924f
Isaac Vander Sluis <ivandersluis001@student.butte.edu>
2017-11-22 02:17:05 +0000

    Lab 4.6 Submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -3,7 +3,11 @@
 
 /*
 
-
+~~~~~~~~~TRANSCRIPT WEBPAGE GENERATOR~~~~~~~~~~~
+This program prompts the user for a file to read. It iterates through the file,
+reading in student names, the number of classes they take, and their respective
+grades. It then calculates GPA and outputs this figure along with their name to
+the user.
 
 */
 
@@ -17,7 +21,8 @@ int main() {
     ifstream inFS;
     ofstream outFS;
     string file_name = "";
-    string student_name = "";
+    string first_name = "";
+    string last_name = "";
     int num_classes = 0;
     char grade = ' ';
     int class_hours = 0.0;
@@ -30,6 +35,7 @@ int main() {
     
     cout << "Opening " << file_name << endl;
     inFS.open(file_name);
+    outFS.open("output.html");
     if (!inFS.is_open()) {
         cout << "Could not open file " << file_name << endl;
         return 1;
@@ -44,10 +50,12 @@ int main() {
     outFS << "<h1>Student Transcript</h1>";
     
     while (!inFS.eof()) {
-        getline(inFS, student_name);
+        sem_gpa = 0.0;
+        inFS >> first_name;
+        inFS >> last_name;
         inFS >> num_classes;
         
-        outFS << "<h3>Student: " << student_name << "</h3>" << endl;
+        outFS << "<h3>Student: " << first_name << " " << last_name << "</h3>" << endl;
         
         for (int n = num_classes; n > 0; n--) {
             inFS >> class_hours;
@@ -75,12 +83,11 @@ int main() {
                     sem_gpa += 0.0;
                     break;
                 default:
-                    cout << "ERROR" << endl;
+                    outFS << "ERROR" << endl;
             }
         }
         
         outFS << "<p>Semester GPA: " << sem_gpa / num_classes << "</p>" <<endl;
-        
     }
     
     outFS << "</body>" << endl;
@@ -89,14 +96,28 @@ int main() {
     cout << "Closing " << file_name << endl;
     
     inFS.close();
+    outFS.close();
     
     return 0;
 }
 
 /*
 
-Enter file name for inport:
-test_input.txt
-Opening test_input.txt
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Transcript</title>
+</head>
+<body>
+<h1>Student Transcript</h1><h3>Student: Suzy Cue</h3>
+<p>Semester GPA: 3.75</p>
+<h3>Student: Joe Joseph</h3>
+<p>Semester GPA: 2</p>
+<h3>Student: Jillian Smith</h3>
+<p>Semester GPA: 2.2</p>
+<h3>Student: Jillian Smith</h3>
+<p>Semester GPA: 2</p>
+</body>
+</html>
 
 */
